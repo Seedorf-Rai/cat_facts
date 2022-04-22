7f5f6cf1ea033a38338899285f30f0d3da276344
Seedorf-Rai <dorf123see@gmail.com>
2022-04-22 14:05:34 +0545

    remote service for jokes created

--- lib/remote_service/remote_service.dart
+++ lib/remote_service/remote_service.dart
@@ -1,4 +1,5 @@
 import 'package:cat_facts/model/cat_model.dart';
+import 'package:cat_facts/model/joke_model.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 
@@ -12,6 +13,21 @@ class RemotesService {
         var jsonString = response.body;
         return catModelFromJson(jsonString);
       } else {
+        return null;
+      }
+    } catch (e) {
+      Get.snackbar("Message", e.toString());
+    }
+  }
+
+  static Future<JokeModel?> fetchjokes() async {
+    try {
+      var response = await client
+          .get(Uri.parse("https://api.chucknorris.io/jokes/random"));
+      if (response.statusCode == 200) {
+        var jsonString = response.body;
+        return jokeModelFromJson(jsonString);
+      }else{
         return null;
       }
     } catch (e) {
